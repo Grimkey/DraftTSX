cd1ef886767739584609a4dfd9e589ec903326d6
James White <Grimkey@gmail.com>
2020-10-13 09:15:15 -0700

    Revert "removing code so make a code review"
    This reverts commit e04bd28ab8d48f441b26971e78b8463f2b7b5879.

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    roots: [
+        "<rootDir>/modules",
+        "<rootDir>/components"
+    ],
+    testEnvironment: "node",
+    globals: {
+        "ts-jest": {
+            diagnostics: {
+                warnOnly: true
+            },
+            tsConfig: 'tsconfig.test.json'
+        }
+    },
+    testMatch: [
+        "**/__tests__/**/*.+(ts|tsx|js)",
+        "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    // Setup Enzyme
+    snapshotSerializers: ["enzyme-to-json/serializer"],
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+}
